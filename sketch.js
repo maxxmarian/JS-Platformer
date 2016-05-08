721c8164a1065107f136573eed87442062bb1c41
Jeffrey P. Bordiga <jeffrey.bordiga@sonomaacademy.org>
2016-05-07 17:26:11 -0700

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,1 +1,14 @@
+  var playerCharacter;
 
+
+Crafty.init(1366, 768, document.getElementById('game'));
+
+playerCharacter = Crafty.e('2D, Canvas, Color, Twoway, Gravity')
+  .attr({x: 0, y: 0, w: 50, h: 50})
+  .color('#F00')
+  .twoway(200)
+  .gravity('Floor');
+
+Crafty.e('Floor, 2D, Canvas, Color')
+  .attr({x: 0, y: 250, w: 250, h: 10})
+  .color('green');
