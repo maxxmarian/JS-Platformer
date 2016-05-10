3baafbc6f8d84355f682ce24c6a2f2740073cf31
Jeffrey P. Bordiga <jeffrey.bordiga@sonomaacademy.org>
2016-05-10 08:13:38 -0700

    kek

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
-
+var windowWidth = 1335;
+var windowHeight = 625;
 var playerCharacter;
 
 
-Crafty.init(1366, 768, document.getElementById('game')); // setup
+Crafty.init(windowWidth, windowHeight, document.getElementById('game')); // setup
+
+Crafty.background(170);
 
 playerCharacter = Crafty.e('2D, Canvas, Color, Twoway, Gravity')
   .attr({x: 0, y: 0, w: 50, h: 50})
@@ -11,6 +14,5 @@ playerCharacter = Crafty.e('2D, Canvas, Color, Twoway, Gravity')
   .gravity('Floor');
 
 Crafty.e('Floor, 2D, Canvas, Color')
-  .attr({x: 0, y: 756, w: 1366, h: 10})
-  .color('#F00');
-//test for commit
+  .attr({x: 0, y: windowHeight - 25, w: windowWidth, h: 25})
+  .color(255, 100, 150);
