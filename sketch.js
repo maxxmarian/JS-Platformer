63cde2a96ff7fd81dc8556533cffb922f8391f2a
Jeffrey P. Bordiga <jeffrey.bordiga@sonomaacademy.org>
2016-05-10 08:43:29 -0700

    added new elements

--- sketch.js
+++ sketch.js
@@ -1,14 +1,28 @@
+var windowWidth = 1335;
+var windowHeight = 625;
 var playerCharacter;
 
 
-Crafty.init(1366, 768, document.getElementById('game')); // setup
+Crafty.init(windowWidth, windowHeight, document.getElementById('game')); // setup
+
+Crafty.background(170);
 
 playerCharacter = Crafty.e('2D, Canvas, Color, Twoway, Gravity')
   .attr({x: 0, y: 0, w: 50, h: 50})
   .color('#F00')
-  .twoway(200)
+  .twoway(400)
+  .jumpSpeed(550)
+  .gravityConst(1000)
   .gravity('Floor');
 
 Crafty.e('Floor, 2D, Canvas, Color')
-  .attr({x: 0, y: 756, w: 1366, h: 10})
-  .color('#F00');
+  .attr({x: 0, y: windowHeight - 25, w: windowWidth, h: 25})
+  .color(255, 100, 150);
+
+Crafty.e('Floor, 2D, Canvas, Color')
+  .attr({x: 200, y: 450, w: 350, h: 25})
+  .color(255, 100, 150);
+
+Crafty.e('Floor, 2D, Canvas, Color')
+  .attr({x: 700, y: 450, w: 350, h: 25})
+  .color(255, 100, 150);
