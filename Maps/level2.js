772d78eb6f15821c0bbcfd9364f7823e8e264cdb
Jeffrey P. Bordiga <jeffrey.bordiga@sonomaacademy.org>
2016-05-23 10:42:19 -0700

    adsflksadhfd

--- Maps/level2.js
+++ Maps/level2.js
@@ -7,7 +7,11 @@ function level2(){
   Crafty.viewport.bounds = {min: {x: 0, y: 0}, max: {x: 3025, y: 3025}}
 
   Crafty.e('Floor, 2D, Canvas, Color') // 1
-    .attr({x: 0, y: 500, w: 2600, h: 25})
+    .attr({x: 0, y: 300, w: 300, h: 25})
+    .color(r, g, b);
+
+  Crafty.e('Wall, 2D, Canvas, Color') // 2
+    .attr({x: 0, y: 300, w: 300, h: 25})
     .color(r, g, b);
 
   for(i = 0; i < 5; i++){
